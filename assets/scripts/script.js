565b7b07d020ff2e6b44d4d9ff6096662db7eb71
StaticCloud <diegomarrs@gmail.com>
2021-11-17 22:57:22 -0800

    css overhaul + added welcome page

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,35 @@
-console.log("Hello world!");
+// get the container element
+var containerEl = document.querySelector(".page-container");
+
+// our function for switching to the appropriate page
+var setPage = function(page) {
+    // render the right page depending on the value of "page"
+    switch(page) {
+        case("welcome"):
+        renderWelcomePage();
+        break;
+    }
+}
+
+// render our welcome page
+var renderWelcomePage = function() {
+    // set the attribute of our container element to welcome for styling purposes
+    containerEl.setAttribute("data-page", "welcome");
+    
+    // create heading, paragraph, and button elements
+    var headingTextEl = document.createElement("h1");
+    headingTextEl.innerHTML = "Coding Quiz!";
+    
+    var paragraphTextEl = document.createElement("p");
+    paragraphTextEl.innerHTML = "Can you answer these 5 code-related questions in under 60 seconds? Answer questions correctly to receive points! Any question answered incorrectly removes 5 seconds from the timer.";
+
+    var startButtonEl = document.createElement("button");
+    startButtonEl.textContent = "Start!";
+
+    // add to the page
+    containerEl.appendChild(headingTextEl);
+    containerEl.appendChild(paragraphTextEl);
+    containerEl.appendChild(startButtonEl);
+}
+
+setPage("welcome");
